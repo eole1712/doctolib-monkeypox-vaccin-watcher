4965c25d52b9efc0042c3b534f9d2c0bf25107e9
Grisha Ghukasyan <grisha@brigad.co>
2022-07-22 00:30:53 +0200

    feat: doctolib watch

--- README.md
+++ README.md
@@ -1,18 +1,14 @@
-# [📔 Typescript minimal starter](https://github.com/DeathVenom54/typescript-minimal-starter)
+# [📔 Doctolib Monkeypox Vaccin Watcher](https://github.com/DeathVenom54/typescript-minimal-starter)
 
-A minimal starter template for a Typescript node.js project
+A simple script to alert you when there are Monkeypox vaccines available in the next three days in Paris
+
+Edit the URL in index.ts to watch for more availability.
 
 ## Ingredients
 
 - [typescript](https://npmjs.com/package/typescript)
 - [prettier](https://npmjs.com/package/prettier)
 - [@types/node](https://npmjs.com/package/@types/node)
-
-## Getting started
-
-1. [Make a repository with this template](https://github.com/DeathVenom54/typescript-minimal-starter/generate)
-2. Edit the package.json with your desired stuff
-3. Get cracking with your project!
 
 ## Included scripts
 
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,37 @@
-const message = 'Hello, world!';
-console.log(message);
+import axios from 'axios';
+import notifier from 'notification-for-mac';
+
+export interface DataDoctor {
+  address: string;
+  city: string;
+  zipcode: string;
+  name_with_title: string;
+  link: string;
+}
+
+async function getAvailabilites() {
+  const {
+    data: {
+      data: { doctors },
+    },
+  } = await axios.get<{ data: { doctors?: DataDoctor[] } }>(
+    'https://www.doctolib.fr/vaccination-variole-du-singe/paris.json?availabilities=3',
+  );
+
+  if (doctors && doctors.length > 0) {
+    const message = doctors
+      .map((doctor) => `- ${doctor.name_with_title} - ${doctor.address}, ${doctor.zipcode} (${doctor.link})`)
+      .join('\n');
+
+    console.log(message);
+    notifier('Des vaccins disponibles dans les 3 prochains jours à Paris', {
+      sound: 'Frog',
+      title: 'Vaccins disponibles',
+    });
+  } else {
+    console.log('Vaccins non disponibles');
+  }
+}
+
+setInterval(getAvailabilites, 1000 * 60 * 3);
+process.stdin.resume();
